3111defb23781c3d3cef3d5ddc38846d54f993c2
yashmane0502-stack <yashmane0502@gmail.com>
2026-02-16 10:26:43 +0530

    salary_prediction_deployment.py

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/149socUlxLXbglxT496-aoWj3eMDqZxZY
 """
 
-!pip install -q streamlit
 import streamlit as st
 import pandas as pd
 import joblib
